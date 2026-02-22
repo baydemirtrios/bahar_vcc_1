98ba18f4e7210007bf7dca4e4ebabbfaeef8bc92
Bahar Aydemir <bahar.aydemir@triosstudent.com>
2026-02-22 01:30:38 -0500

    I love life

---
+++ script.js
@@ -0,0 +1,5 @@
+// Get references to elements
+const firstNameInput = document.getElementById('firstName');
+const lastNameInput = document.getElementById('lastName');
+const submitBtn = document.getElementById('submitBtn');
+const greeting = document.getElementById('greeting');
