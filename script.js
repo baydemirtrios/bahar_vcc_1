8b5912c40f77a570ab3e246404a86473a3c58ece
Bahar Aydemir <bahar.aydemir@triosstudent.com>
2026-02-22 01:47:04 -0500

    Add HTML and JS files

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Get references to elements
 const firstNameInput = document.getElementById('firstName');
-const lastNameInput = document.getElementById('lastName');
+const lastNameInput = document.getElementById('lastNameandnumber');
 const submitBtn = document.getElementById('submitBtn');
+const greeting = document.getElementById('greeting');
 const greeting = document.getElementById('greeting');
